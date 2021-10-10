fbe807534ab7554f215c90d37e7e38e0201325e4
Ahmad Batwa <ah444mad@hotmail.com>
2021-10-11 00:52:07 +0300

    Updated the design of dataPath Regarding the problem in previous commit, I changed the design of dp. Now, the multiplexers are connected to the counter, not the registers.
    This eliminated the previous problem. But now the TMV shows no adder.
    It appears as if the adder was augmented within one of the registers.

--- controlUnit.v
+++ controlUnit.v
@@ -4,7 +4,7 @@ module controlUnit(
 	// feedback from data path
 	input [4:0] counter, count_to,
 	// output control signals
-	output reg counter_reset, mux_sel, load_output, counter_enb, done);
+	output reg internal_reset, mux_sel, load_output, counter_enb, done);
 	
 	
 	reg[1:0] state;
@@ -18,20 +18,20 @@ module controlUnit(
 	
 	always @(posedge clk or negedge reset) begin
 		
-		if(~reset) begin					// Reset
+		if(~reset) begin									// Reset
 			state <= 0;
 		end
 		
-		else begin							// Clock edge
+		else begin											// Clock edge
 			state <= nextState;
 		end
 	end
 		
 	always @(*) begin
-		mux_sel = 1; // default start value
-		counter_reset = 0; // by default the counter is resetted????????? or just use the enable?
+		mux_sel = 1; 										// default start value (0 and 1)
+		internal_reset = 1;								// resets the counter AND (current and previous) registers
 		load_output = 0;
-		counter_enb = 0; // don't count
+		counter_enb = 0; 
 		done = 0;
 		
 		
@@ -40,8 +40,10 @@ module controlUnit(
 			// State 0: idle, only wait for (go) signal to load the input
 			0: begin
 				// if go is recieved, load the input register and go to state 1
-				if (load_input)
+				if (load_input) begin
+					internal_reset = 0; 					// Reseet any previous values in the counter and regs before proceeding to next state.
 					nextState = state + 1;
+				end
 				else 
 					nextState = state;
 			end
@@ -49,7 +51,7 @@ module controlUnit(
 			// State 1: start counting and summing
 			1: begin
 				mux_sel = 1;								// choose starting values of the sequence (0, 1)
-				counter_reset = 1;						// enable the counter
+				internal_reset = 1;						// counter and regs un-reset
 				counter_enb = 1;							// start counting
 				nextState = state + 1;
 			end
@@ -58,7 +60,6 @@ module controlUnit(
 			2: begin
 				mux_sel = 0;								// pass previous and current value of the sequence to the adder
 				counter_enb = 1;
-				counter_reset = 1;
 				if(counter == count_to) begin			// if countr arrive at its value, enable the load signal of output register
 					load_output = 1;
 					nextState = state + 1; 
@@ -69,11 +70,7 @@ module controlUnit(
 			
 			// State 3: make (done) signal high, then sends the controller back to state 0.
 			3: begin
-				done = 1;
-				// no need for the two below, they are the default.
-				counter_reset = 0;						// reset the counter
-				load_output = 0;							// disable loading output register to prevent it from changing after this cycle.
-				
+				done = 1;				
 				nextState = 0;
 			end
 		endcase
--- dataPath.v
+++ dataPath.v
@@ -5,7 +5,7 @@ module dataPath(
 	output [4:0] count,
 	output [15:0] nth_fib 
 	);
-	
+		
 	wire [15:0] sum;	
 	wire [15:0] mux0_out;
 	wire [15:0] mux1_out;
@@ -17,22 +17,21 @@ module dataPath(
 	localparam [15:0] mux1_in1=0;
 	
 	// Input and output registers
-	register_5bit input_reg(clk, usr_reset, load_input, numberIn, count_to);
-	register_16bit output_reg(clk, usr_reset, load_output, current_val, nth_fib);
+	register_5bit input_reg(clk, usr_reset, load_input, numberIn, count_to);//
+	register_16bit output_reg(clk, usr_reset, load_output, current_val, nth_fib);//
 
 	// Counter
-	counter_5bit counter(clk, usr_reset&cu_reset, counter_enb, count);
+	counter_5bit counter(clk, usr_reset&cu_reset, counter_enb, count);//
 	
 	// Current and previous value registers
-	register_16bit current_reg(clk, usr_reset&cu_reset, 1'b1, mux0_out, current_val);  
-	register_16bit prev_reg(clk, usr_reset&cu_reset, 1'b1, mux1_out, prev_val);
+	register_16bit current_reg(clk, usr_reset&cu_reset, 1'b1, sum, current_val);//  
+	register_16bit prev_reg(clk, usr_reset&cu_reset, 1'b1, current_val, prev_val);
 	
-	// Multiplexers
-	// Mux 0 doesn't appear in Technology view map. Investigate why.
-	MUX2_16bit mux0(sum, mux0_in1, first_time, mux0_out); 
-	MUX2_16bit mux1(current_val, mux1_in1, first_time, mux1_out);
+	// Multiplexers will choose what is fed to the adder (either (0 and 1) or (current and previous))
+	MUX2_16bit mux0(current_val, 1'b0 ,first_time, mux0_out); 
+	MUX2_16bit mux1(prev_val, 1'b1 ,first_time, mux1_out);
 	
 	// Sum = current value + previous value.
-	adder_16bit adder(current_val, prev_val, sum);
+	adder_16bit adder(mux0_out, mux1_out, sum);
 
 endmodule
